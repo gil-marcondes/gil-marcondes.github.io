59251c682a6937457ecae0d84b7a7f7e6f4079c2
gil-marcondes <73506561+gil-marcondes@users.noreply.github.com>
2020-12-14 00:11:09 -0300

    Create TemaEstilos.js

---
+++ src/TemaEstilos.js
@@ -0,0 +1,41 @@
+const TemaApp = {
+    light: {
+     color: "#000",
+	 borderStyle: "solid",
+		borderColor: "#000",
+		paddingLeft:50,
+		textAlign: "center",
+
+    },
+    dark: {
+        color: "#fff",
+		borderStyle: "solid",
+		borderColor: "#fff",
+		paddingLeft:50,
+		textAlign: "center",
+    },
+    lightContainer: {
+      paddingTop:300,
+	  paddingBottom:300,
+      backgroundColor: "#fff",
+		
+    },
+    darkContainer: {
+     paddingTop:300,
+	 paddingBottom: 300,
+     backgroundColor: "#333",
+		
+    },
+lightButton:{
+	backgroundColor: "#000",
+	 color: "#fff",
+	 
+} , 
+
+darkButton:{
+	backgroundColor: "#fff",
+	 color: "#000",
+}, 
+
+}
+export default TemaApp;
