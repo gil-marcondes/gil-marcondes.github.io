a44e83e214bb4ae4d7a2f898d3dfcf51c3e3346a
gil-marcondes <73506561+gil-marcondes@users.noreply.github.com>
2020-12-14 00:02:50 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+import Principal from "./EscolherTema";
+
+class App extends Component{
+  render(){
+  return (
+  <div>
+ 
+        <Principal/>
+		
+      </div>
+	  );
+}
+}
+
+export default App;
